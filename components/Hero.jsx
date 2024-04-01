27cebc437a1e7cd7b611e71ec3e8e745ad5204c1
aliouuuw <wadealiou00@gmail.com>
2024-04-01 15:26:24 -0400

    Added animations to hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,11 @@
+"use client"
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
 import Marquee from "./ui/marquee";
 import AnimatedTitle from "./ui/animated-title";
-import AnimatedSubTitle from "./ui/animated-subtitle";
+import { Typewriter } from 'react-simple-typewriter'
 
 export const Hero = () => {
   const items = ["Front-end", "Back-end", "Mobile", "Arduino", "DevOps", "IoT"];
@@ -20,23 +21,51 @@ export const Hero = () => {
               className="rounded-full object-center brightness-95 object-cover"
             />
           </div>
-          <div className="flex gap-x-2">
-            <Link href="https://twitter.com/aliouuuw" target="_blank">
-              <FontAwesomeIcon icon={faXTwitter} className="h-6" />
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/aliou-wade-35a435196/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedin} className="h-6" />
-            </Link>
+          <div className="flex flex-col items-end gap-y-2">
+            <div className="flex items-center justify-end gap-x-2">
+              <div className="animate-ping h-2 w-2 rounded-full bg-green-500" />
+              <p className="animate-pulse text-sm text-green-500">
+                Open for work
+              </p>
+            </div>
+            <div className="flex gap-x-2">
+              <Link href="https://twitter.com/aliouuuw" target="_blank">
+                <FontAwesomeIcon icon={faXTwitter} className="h-6" />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/aliou-wade-35a435196/"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faLinkedin} className="h-6" />
+              </Link>
+            </div>
           </div>
         </div>
-        <AnimatedTitle text="Hi, I'm Aliou" className="text-xl mb-2" x={20} y={10} />
-        <AnimatedSubTitle
-          text="With over 5 years of experience, I've worked with startups and large corporations worldwide, crafting cutting-edge applications and innovative technology products."
-          className="text-sm text-pretty text-neutral-400 "
+        <AnimatedTitle
+          text="Hi, I'm Aliou"
+          className="text-xl mb-2"
+          x={20}
+          y={10}
         />
+        {/* <AnimatedSubTitle
+          text="I'm a Software Engineer having fun with tech. My recent focus has been web and mobile development. Looking to get into AI soon..."
+          className="text-sm text-pretty text-neutral-400 "
+        /> */}
+        <p className="text-sm text-neutral-400">
+         <Typewriter
+            words={[
+              "I'm a Software Engineer having fun with tech...",
+              "My recent focus has been web and mobile development..",
+              "Looking to get into AI soon!",
+            ]}
+            loop={5}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </p>
       </div>
       <Marquee items={items} />
     </div>
