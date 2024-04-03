b7c01467c7ca3c69f8d4759e6f0059f23e52b30e
aliouuuw <wadealiou00@gmail.com>
2024-04-03 16:23:18 -0400

    Restyled Banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -8,13 +8,13 @@ export default function BannerComponent() {
     <Banner className="pt-2 px-8">
       <div className="flex w-full justify-between rounded-2xl border border-neutral-700 shadow-neutral-900 shadow-lg backdrop-blur-md bg-gradient-to-bl from-transparent to-neutral-950 to-90% p-2 ">
         <div className="mx-auto flex items-center">
-          <p className="flex items-center text-sm font-normal text-neutral-400">
+          <div className="flex items-center text-sm text-center ">
             <MdAnnouncement className="m-2 h-4 w-4" />
-            <span className="[&_p]:inline">
-              Working on adding mobile apps to Projects. They&apos;ll be here&nbsp;
-            </span>
+            <p className="max-md:text-xs text-neutral-400">
+              Working on adding mobile apps to Projects. Here&nbsp;
             <span className="text-green-500">soon</span>
-          </p>
+            </p>
+          </div>
         </div>
         <BannerCollapseButton color="gray" className="border-0 bg-transparent text-neutral-400">
           <HiX className="h-4 w-4" />
